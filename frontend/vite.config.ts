47213639593ff724479e0387976c022c5d4c57f2
IBCoordinacionProyectos <coordinacion.proyectos@ingeniobim.co>
2025-08-11 02:58:20 -0500

    v38

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,30 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import viteCompression from 'vite-plugin-compression';
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-
     // Gzip precomprimido
     viteCompression({
       algorithm: 'gzip',
       ext: '.gz',
-      deleteOriginFile: false
+      deleteOriginFile: false,
+      filter: /\.(js|css|html|svg)$/i
     }),
-
     // Brotli precomprimido
     viteCompression({
       algorithm: 'brotliCompress',
       ext: '.br',
-      deleteOriginFile: false
+      deleteOriginFile: false,
+      filter: /\.(js|css|html|svg)$/i
     })
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, 'src'),
     },
   },
   server: { port: 3000 },
