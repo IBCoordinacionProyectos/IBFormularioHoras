5bca5e925402946bc86e68522823457499d19c18
IBCoordinacionProyectos <coordinacion.proyectos@ingeniobim.co>
2025-08-11 02:53:39 -0500

    v37

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import viteCompression from 'vite-plugin-compression';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    // Gzip precomprimido
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      deleteOriginFile: false
+    }),
+
+    // Brotli precomprimido
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      deleteOriginFile: false
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: { port: 3000 }
+  server: { port: 3000 },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild'
+  }
 });
