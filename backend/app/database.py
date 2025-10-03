33d2de38cc3dc609407e86135e866a0bff32912d
IBCoordinacionProyectos <coordinacion.proyectos@ingeniobim.co>
2025-10-03 12:46:47 -0500

    V79 Cambios errores log

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
